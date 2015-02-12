ec67e3bce5aff163b5544018a234ccdc194c571e
Isaac Friedman <ifriedman21@gmail.com>
2015-02-12 11:47:51 -0500

    Added Heapsort
    Uses python native min heap. Might write a max heap at some point

--- study_aid.py
+++ study_aid.py
@@ -1,15 +1,16 @@
 import time
 import random
+import heapq
+
 implemented = []
 #The various sorting algorithms. I've tried to use the simplest implementations for study purposes.
 
-def bubbleSort(bad_list):
+def bubble_sort(bad_list):
 	sorted = False
 	i = 0
 	while not sorted:
 		sorted = True
-		print sorted
-		print "Each time we go through the array, we will assume it's already sorted until we find an element out of place."
+		print("Each time we go through the array, we will assume it's already sorted until we find an element out of place.")
 		for n in range(len(bad_list)-1):
 			if bad_list[n] > bad_list[n+1]:
 				sorted = False
@@ -17,30 +18,20 @@ def bubbleSort(bad_list):
 				bad_list[n], bad_list[n+1] = swap(bad_list[n], bad_list[n+1])
 				print ("Swapped:", bad_list[n], bad_list[n+1])
 			else:
-				print bad_list[n]
-		print "Now the array looks like this: \n", bad_list
+				print(bad_list[n])
+		print ("Now the array looks like this: \n", bad_list)
 	return bad_list
 
-def selectionSort(bad_list):
-
+def selection_sort(bad_list):
 	for i in range(len(bad_list)):
 		curr_min = min(bad_list[i:])
 		mindex = bad_list[i:].index(curr_min)
 		bad_list[i + mindex] = bad_list[i]
 		bad_list[i] = curr_min #no function call necessary
-		print bad_list
-		'''
-	source = [4,2,1,10,5,3,100]
-	for i in range(len(source)):
-		mini = min(source[i:]) #find minimum element
-		min_index = source[i:].index(mini) #find index of minimum element
-		source[i + min_index] = source[i] #replace element at min_index with first element
-		source[i] = mini                  #replace first element with min element
-	print source
-		'''
+		print(bad_list)
 	return bad_list
 			
-def insertionSort(bad_list):
+def insertion_sort(bad_list):
 	for i in range(1, len(bad_list) - 1):
 		x = bad_list[i]
 		j = i 
@@ -48,9 +39,25 @@ def insertionSort(bad_list):
 			bad_list[j] = bad_list[j-1]
 			j -= 1
 		bad_list[j] = x
-		print bad_list
+		print(bad_list)
 	return bad_list
 
+
+'''
+def merge_sort(bad_list):
+	print "Coming soon"
+
+def shell_sort(bad_list):
+	print "Coming soon"
+'''
+
+def heap_sort(bad_list):
+	#Note that we are using python's native min heap instead of the max heap used in textbooks. Maybe I'll write a max heap for python at some future date.
+	h= []
+	for val in bad_list:
+		heapq.heappush(h, val)
+		print(h)
+	return [heapq.heappop(h) for i in range(len(h))]
 
 #Utility functions used by the various algorithms. Allegedly, good practice is "If you use it more than twice, put it in a function"
 def swap(a, b):
@@ -58,31 +65,27 @@ def swap(a, b):
 	localA, localB = b, a
 	return localA, localB
 	
-print "Hello Wrold! \n\n"
-time.sleep(0.75)
-print "This script is designed to illustrate various sorting algorithms that Ysoschor Fried \n asked me about while studying for a final exam in data structures."
-time.sleep(0.75)
-print "It is also designed to allow me to play with Python.\n"
+print("This script is designed to illustrate various sorting algorithms that Ysoschor Fried \n asked me about while studying for a final exam in data structures.")
 time.sleep(0.75)
 
-funcs = [bubbleSort, selectionSort, insertionSort]
+funcs = [bubble_sort, selection_sort, insertion_sort, heap_sort]
 
 while True:
-	print "A loop generates a bunch of random integers 0 <= N <= 500 to be sorted. \n\n"
+	print("A loop generates a bunch of random integers 0 <= N <= 500 to be sorted. \n\n")
 	a = random.sample(range(0, 500), 10)
 	aString = ', '.join(map(str, a)) 
-	print aString
-	print "\n\n This script generates a new array each time you want to try out a new algorithm."
-	print"\n The following Sorts have been illustrated so far. Enter the number of the sort you wish to watch: \n"
+	print (aString)
+	print ("\n\nThis script generates a new array each time you want to try out a new algorithm.")
+	print("\nThe following Sorts have been illustrated so far. Enter the number of the sort you wish to watch: \n")
 	for i in range(len(funcs)):
-		print i, "\t", str(funcs[i])
+		print (i, "\t", str(funcs[i]))
 	call = input("Call a func by number: ")
 	try:
 		int(call)
-		print "You called function ", call
+		print ("You called function ", call, "\n\n")
 		b = funcs[int(call)](a)
 	except:
-		print call, " has not been implemented yet, or is an invalid input. \nInput is valid if and only if it is a number from the left column in the above list."
-		print "Try again."
+		print (call, " has not been implemented yet, or is an invalid input. \nInput is valid if and only if it is a number from the left column in the above list.")
+		print ("Try again.")
 		time.sleep(0.5)
 	pass
